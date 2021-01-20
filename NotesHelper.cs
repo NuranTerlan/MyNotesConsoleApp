178624cdc5630d5d1b33fa99356bde7346c65a37
Nuran Terlan <nuran.terlanli@asoiu.edu.az>
2021-01-21 02:28:37 +0400

    NotesHelper class created

---
+++ NotesHelper.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using MoreLinq;
+using MyNotesConsoleApp.Data.Entities;
+
+namespace MyNotesConsoleApp
+{
+    public static class NotesHelper
+    {
+        public static void NoteLogger(Note note)
+        {
+            string updatedTime = note.UpdatedAt == null
+                ? ""
+                : $"\nUpdated : {note.UpdatedAt}";
+            Console.WriteLine($"\n---------------\nID : {note.Id}\nTitle : {note.Title}\nNote : {note.Content}\nCreated : {note.CreatedAt}{updatedTime}");
+            note.NoteTags.ForEach(nt => Console.Write($"#{nt.Tag.Name} "));
+            Console.WriteLine("\n---------------");
+        }
+    }
+}
