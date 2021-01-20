099b38ef8acfd9e39b443e451020eacf3158638f
Nuran Terlan <nuran.terlanli@asoiu.edu.az>
2021-01-21 02:14:21 +0400

    new helper methods are added

--- CommandHelper.cs
+++ CommandHelper.cs
@@ -11,7 +11,7 @@ namespace MyNotesConsoleApp
 
             Console.ForegroundColor = ConsoleColor.Green;
             Console.WriteLine($"\n {commandName} commands to use this program : \n");
-            Console.ForegroundColor = ConsoleColor.Yellow;
+            Console.ForegroundColor = ConsoleColor.White;
             foreach (var (key, value) in commands)
             {
                 Console.WriteLine($"    {key} ( {value} )");
@@ -24,8 +24,6 @@ namespace MyNotesConsoleApp
             Console.Write("\n~command/ ");
             Console.ForegroundColor = ConsoleColor.White;
             string command = Console.ReadLine();
-            Console.WriteLine($"\nYour command is {command}\n");
-            Console.ForegroundColor = ConsoleColor.Cyan;
 
             return command;
         }
@@ -33,9 +31,7 @@ namespace MyNotesConsoleApp
         public static string AskAndReturnVariable(string question, bool isRequired = false)
         {
             string required = isRequired ? "required" : "optional";
-            Console.ForegroundColor = ConsoleColor.Green;
             Console.Write($"{question} ({required}) : ");
-            Console.ForegroundColor = ConsoleColor.White;
             string answer = Console.ReadLine();
 
             return answer;
@@ -49,18 +45,101 @@ namespace MyNotesConsoleApp
             return answer;
         }
 
+        public static void Success(string successMsg)
+        {
+            Console.ForegroundColor = ConsoleColor.Green;
+            Console.WriteLine($"{successMsg} successfully --------");
+            Console.ForegroundColor = ConsoleColor.White;
+        }
+
         public static void Wrong(string command)
         {
             Console.ForegroundColor = ConsoleColor.Red;
             Console.WriteLine($"{command} is invalid command. Try again!");
-            Console.ForegroundColor = ConsoleColor.Green;
+            Console.ForegroundColor = ConsoleColor.White;
         }
 
         public static void Error(string content)
         {
             Console.ForegroundColor = ConsoleColor.Red;
             Console.WriteLine($"{content}!");
-            Console.ForegroundColor = ConsoleColor.Green;
+            Console.ForegroundColor = ConsoleColor.White;
+        }
+
+        public static string EditProp(string propName, string prop)
+        {
+            string askForProp = YesOrNo($"Do you want to change {propName}");
+            string editedString;
+            switch (askForProp)
+            {
+                case "y":
+                    Console.Write($"{propName} : ");
+                    editedString = ReadLine(prop);
+                    break;
+                case "n":
+                    Console.WriteLine("Default is keeping!");
+                    editedString = prop;
+                    break;
+                default:
+                    Error($"{askForProp} is incorrect answer type!");
+                    Console.WriteLine("But we are keeping default!");
+                    editedString = prop;
+                    break;
+            }
+
+            return editedString;
+        }
+
+        public static string ReadLine(string defaultStr = "default")
+        {
+            int pos = Console.CursorLeft;
+            int initPos = pos;
+            ConsoleKeyInfo info;
+            List<char> chars = new List<char>();
+            if (string.IsNullOrEmpty(defaultStr) == false)
+            {
+                chars.AddRange(defaultStr.ToCharArray());
+            }
+
+            while (true)
+            {
+                Console.CursorLeft = initPos;
+                foreach (var c in chars)
+                {
+                    Console.Write(c);
+                }
+                Console.Write(' ');
+                Console.CursorLeft = pos + 1;
+
+                info = Console.ReadKey(true);
+
+                if (info.Key == ConsoleKey.Backspace 
+                    && Console.CursorLeft > initPos
+                    && Console.CursorLeft <= (initPos + chars.Count))
+                {
+                    chars.RemoveAt(pos - initPos);
+                    if (pos > initPos) pos--;
+                }
+                else if (info.Key == ConsoleKey.Spacebar)
+                {
+                    pos++;
+                    chars.Insert(pos - initPos, ' ');
+                }
+                else if (char.IsLetterOrDigit(info.KeyChar))
+                {
+                    pos++;
+                    chars.Insert(pos - initPos, info.KeyChar);
+                }
+                else if (info.Key == ConsoleKey.LeftArrow && Console.CursorLeft > initPos) pos--;
+                else if (info.Key == ConsoleKey.RightArrow 
+                         && Console.CursorLeft >= initPos 
+                         && Console.CursorLeft < (initPos + chars.Count)) pos++;
+                else if (info.Key == ConsoleKey.Enter) break;
+            }
+
+            string finalRes = new string(chars.ToArray());
+
+            return finalRes;
         }
     }
 }
