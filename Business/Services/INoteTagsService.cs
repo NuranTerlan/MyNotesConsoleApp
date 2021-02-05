0058a63f5cdcc03242fc87b93feb55accf2d2743
Nuran Terlan <nuran.terlanli@asoiu.edu.az>
2021-02-05 21:19:41 +0400

    Update INoteTagsService.cs

--- Business/Services/INoteTagsService.cs
+++ Business/Services/INoteTagsService.cs
@@ -1,9 +1,11 @@
-﻿using MyNotesConsoleApp.Data.Entities;
+﻿using System.Threading.Tasks;
+using MyNotesConsoleApp.Data.Entities;
 
 namespace MyNotesConsoleApp.Business.Services
 {
     public interface INoteTagsService
     {
-        
+
+        Task<bool> CreateAsync(NoteTag noteTag);
     }
 }
