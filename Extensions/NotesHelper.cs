73e68e14e59d678f95e54cd112db9c7c46358185
Nuran Terlan <nuran.terlanli@asoiu.edu.az>
2021-02-07 05:18:41 +0400

    NotesHelper is extension class

---
+++ Extensions/NotesHelper.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using MoreLinq;
+using MyNotesConsoleApp.Data.Entities;
+
+namespace MyNotesConsoleApp.Extensions
+{
+    public static class NotesHelper
+    {
+        public static void NoteLogger(Note note)
+        {
+            string updatedTime = note.UpdatedAt == null
+                ? ""
+                : $"\nUpdated : {note.UpdatedAt}";
+            Console.WriteLine($"\n---------------\nID : {note.Id}\nTitle : {note.Title}" +
+                              $"\nNote : {note.Content}\nCreated : {note.CreatedAt}{updatedTime}");
+            note.NoteTags.ForEach(nt => Console.Write($"#{nt.Tag.Name} "));
+            Console.WriteLine("\n---------------");
+        }
+    }
+}
